b032d7d7f353422588fc77a9b028788742bd02a7
Isak AGBLE <i.am.agble@gmail.com>
2026-02-16 21:45:38 +0000

    hero section model created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Hero_Section(models.Model):
+    hero_text_title = models.CharField(max_length=100)
+    hero_text_body = models.TextField()
+    hero_image = models.ImageField(upload_to='main/images/')
+
+    def __str__(self):
+        return self.hero_text_title
+    
+
+
