8d7fd7daa129c0826d94890b7562ac51af32b213
Isak AGBLE <i.am.agble@gmail.com>
2026-03-01 09:06:27 +0000

    callback function moved to js file and captcha added to login page

---
+++ main/static/main/js/script.js
@@ -0,0 +1,8 @@
+// Google Recapture Script For Callback 
+function enableSubmit() {
+    document.getElementById("submit").disabled = false;
+}
+
+function disableSubmit() {
+    document.getElementById("submit").disabled = true;
+}
